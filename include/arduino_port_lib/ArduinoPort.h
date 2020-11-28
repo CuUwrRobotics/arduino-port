77601d4d1fe3c73b2d03b8b49640d8992bb2a8a6
Nicholas Steele <nichlock@gmail.com>
2020-11-28 14:45:07 -0500

    Add min() and max() functions to replace those in Arduino.h

--- include/arduino_port_lib/ArduinoPort.h
+++ include/arduino_port_lib/ArduinoPort.h
@@ -4,4 +4,7 @@
 // Arduino millisecond delay function to linux microsecond sleep function.
 #define delay(a) usleep(a * 1000)
 
+#define min(a, b) ((a >= b)? a:b)
+#define max(a, b) ((a <= b)? a:b)
+
 #endif /* ifndef ARDUINOPORT_H */
