91fa84649ca939b2ba8181ff6889949e52b2564f
Nicholas Steele <nichlock@gmail.com>
2020-12-16 16:15:35 -0500

    Added some standard bitwise and basic math operations from arduino.h

--- include/arduino_port_lib/ArduinoPort.h
+++ include/arduino_port_lib/ArduinoPort.h
@@ -4,7 +4,36 @@
 // Arduino millisecond delay function to linux microsecond sleep function.
 #define delay(a) usleep(a * 1000)
 
-#define min(a, b) ((a >= b)? a:b)
-#define max(a, b) ((a <= b)? a:b)
+// undefine stdlib's abs if encountered
+#ifdef abs
+#undef abs
+#endif /* ifdef abs */
+
+#define min(a, b) ((a) < (b)?(a):(b))
+#define max(a, b) ((a) > (b)?(a):(b))
+#define abs(x) ((x) > 0?(x):-(x))
+#define constrain(amt, low, high) ((amt) < (low)?(low):((amt) > (high)?(high): \
+                                                        (amt)))
+#define round(x)     ((x) >= 0?(long)((x) + 0.5):(long)((x) - 0.5))
+#define radians(deg) ((deg) * DEG_TO_RAD)
+#define degrees(rad) ((rad) * RAD_TO_DEG)
+#define sq(x) ((x) * (x))
+
+#define interrupts() sei()
+#define noInterrupts() cli()
+
+#define clockCyclesPerMicrosecond() (F_CPU / 1000000L)
+#define clockCyclesToMicroseconds(a) ((a) / clockCyclesPerMicrosecond())
+#define microsecondsToClockCycles(a) ((a) * clockCyclesPerMicrosecond())
+
+#define lowByte(w) ((uint8_t) ((w) & 0xff))
+#define highByte(w) ((uint8_t) ((w) >> 8))
+
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitToggle(value, bit) ((value) ^= (1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet(value, bit) : \
+                                        bitClear(value, bit))
 
 #endif /* ifndef ARDUINOPORT_H */
